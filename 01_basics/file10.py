5abdebbe835dc9d70306e956f93211d46130542a
PiyushPanwarFST <piyush2002panwar@gmail.com>
2025-03-22 17:51:43 +0530

    modified

--- 01_basics/file10.py
+++ 01_basics/file10.py
@@ -1,1 +1,14 @@
 print("this is file no 10")
+
+
+
+# from arviz_stats import bayes_factor
+
+# # Compute Bayes Factor for Model A and Model B
+# bf = bayes_factor(model_A, model_B)
+
+# # Make a decision based on the result
+# if bf > 1:
+#     print("Model A (simple design) fits the data better.")
+# else:
+#     print("Model B (detailed design) fits the data better.")
