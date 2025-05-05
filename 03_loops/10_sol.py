ce194e59048a6065d502b8180f655942f1907b9d
PiyushPanwarFST <piyush2002panwar@gmail.com>
2025-05-05 14:51:42 +0530

    python

--- 03_loops/10_sol.py
+++ 03_loops/10_sol.py
@@ -1,1 +1,11 @@
-"my name is piyush panwar"
+import time
+
+wait_time = 1
+max_retries = 5
+attemps = 0
+
+while attemps < max_retries:
+    attemps += 1
+    wait_time *= 2
+    print(f"Attempt {attemps}: Waiting for {wait_time} seconds...")
+    time.sleep(wait_time)
